d6637e5f61a152420828de1812d11c54a9fc15a7
N.Ilya <nekrasov@greensight.ru>
2015-01-09 12:57:33 +0000

    Initial workbench package

---
+++ src/Arrilot/Widget/WidgetServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Arrilot\Widget;
+
+use Illuminate\Support\ServiceProvider;
+
+class WidgetServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('arrilot/widget');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
