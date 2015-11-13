8f0fbd88b9a709127a6350cb1b98783fc30ba862
Roman Kinyakin <1@grep.su>
2015-11-13 12:14:02 +0600

    Use HtmlString instead of deprecated Expression

--- src/Misc/ViewExpressionTrait.php
+++ src/Misc/ViewExpressionTrait.php
@@ -2,21 +2,21 @@
 
 namespace Arrilot\Widgets\Misc;
 
-use Illuminate\View\Expression;
+use Illuminate\Support\HtmlString;
 
 trait ViewExpressionTrait
 {
     /**
-     * Convert a given html to View Expression object that was introduced in Laravel 5.1.
+     * Convert a given html to HtmlString object that was introduced in Laravel 5.1.
      *
      * @param string $html
      *
-     * @return \Illuminate\View\Expression|string
+     * @return \Illuminate\Support\HtmlString|string
      */
     protected function convertToViewExpression($html)
     {
-        if (interface_exists('Illuminate\Contracts\Support\Htmlable') && class_exists('Illuminate\View\Expression')) {
-            return new Expression($html);
+        if (interface_exists('Illuminate\Contracts\Support\Htmlable') && class_exists('Illuminate\Support\HtmlString')) {
+            return new HtmlString($html);
         }
 
         return $html;
