d33f1e7dd382653b1902039339c2e5c8aa3e4c75
Ben Rowe <ben.rowe.83@gmail.com>
2016-06-01 18:31:16 +1000

    Add WidgetExpression
    Supports the standard laravel view expression, but also contains an instance of the widget so that the widgets public methods can be exposed into the view

---
+++ src/Misc/WidgetExpression.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Arrilot\Widgets\Misc;
+
+use Illuminate\View\Expression;
+use Arrilot\Widgets\AbstractWidget;
+
+/**
+ * Represents an widget to the view
+ */
+class WidgetExpression extends Expression
+{
+    protected $widget;
+
+    /**
+     * @inheritdoc
+     * @param AbstractWidget $widget instance of the widget this expression
+     *                               represents
+     */
+    public function __construct($html, AbstractWidget $widget)
+    {
+        $this->widget = $widget;
+        parent::__construct($html);
+
+    }
+
+    /**
+     * Get the instance of the widget this expression represents
+     *
+     * @return AbstractWidget
+     */
+    public function getWidget()
+    {
+        return $this->widget;
+    }
+
+    /**
+     * Call a method directly on the widget instance
+     *
+     * @param  string $method method name
+     * @param  array $params
+     * @return mixed
+     */
+    public function __call($method, array $params = [])
+    {
+        if (is_callable($this->widget, $method)) {
+            return call_user_func_array([$this->widget, $method], $params);
+        }
+        throw new InvalidArgumentException(
+            sprintf(
+                '"%s" does not have a method of "%s"',
+                get_class($this->widget),
+                $method
+            )
+        );
+    }
+}
