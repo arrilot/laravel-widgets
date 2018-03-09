3e0cf1bc578870f9c7db9893aaef17f8cd7dd0d7
Sebastien HEYD <sebastien.heyd@plus-que-pro.fr>
2018-03-09 10:48:18 +0100

    Check if current cache driver can use tags

--- src/Misc/LaravelApplicationWrapper.php
+++ src/Misc/LaravelApplicationWrapper.php
@@ -33,7 +33,13 @@ class LaravelApplicationWrapper implements ApplicationWrapperContract
      */
     public function cache($key, $minutes, $tags, Closure $callback)
     {
-        return $this->app->make('cache')->tags($tags)->remember($key, $minutes, $callback);
+        $cache = $this->app->make('cache');
+
+        if(method_exists($cache->getStore(), 'tags')) {
+            $cache = $cache->tags($tags);
+        }
+
+        return $cache->remember($key, $minutes, $callback);
     }
 
     /**
